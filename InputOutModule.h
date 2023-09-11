c1ae06868ab158661e70327f953d8a3ae91f6069
Эстокадо Фамилио <mimimilka55@gmail.com>
2023-09-11 21:09:54 +0500

    Промежуточные попытки

---
+++ InputOutModule.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+class InputOutModule
+{
+public:
+	unsigned linenumber; // Номер строки
+	unsigned charnumber; // Номер символа в строке
+	char nextChar()
+	{
+		if (linenumber == LastInLine)
+		{
+
+		}
+	}
+private:
+
+};
+
