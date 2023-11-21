caac5f4376b65f383c78806b09872196ebd1b6e4
Эстокадо Фамилио <mimimilka55@gmail.com>
2023-11-21 22:38:38 +0500

    some syntax

--- Compiler.cpp
+++ Compiler.cpp
@@ -6,12 +6,7 @@ int main() {
 	shared_ptr<Lexer> lexer = make_shared<Lexer>();
 	shared_ptr<Syntax> syntax = make_shared<Syntax>(lexer);
 	std ::shared_ptr<Token> token = nullptr;
-	do {
-		token = lexer -> getNextToken();
-		if (token) token->Print();
-		syntax -> run_syntax();
-		cout << '\n';
-	} while (token);
+	syntax->run_syntax();
 
 	return 0;
 }
--- Syntax.h
+++ Syntax.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include "ÑToken.h"
+#include <map>
 class Syntax
 {
 public:
@@ -17,155 +18,99 @@ private:
 	void getNext() {
 		this->curToken = this->lexer->getNextToken();
 	}
-	void accept(eSpecialSymbols ss)
+	bool accept(eSpecialSymbols ss)
 	{
 		// For SpecialSumbols
 		try
 		{
-			if (curToken == NULL || curToken->type != ttSpecialSymbols || dynamic_cast<SpecialSymbols*>(curToken.get())->ss != ss)
-			{
-				throw exception("expected another op");
-			}
+			if (curToken == NULL || curToken->type != ttSpecialSymbols || dynamic_cast<SpecialSymbols*>(curToken.get())->ss != ss) throw exception("expected spec symbol op\n");
+			else getNext();
 		}
 		catch (const std::exception& exp)
 		{
 			cout << exp.what();
+			return false;
 		}
+		return true;
 	}
-	void accept(eKeyWords kw)
+	bool accept(eKeyWords kw)
 	{
 		// For KeyWords
 		try
 		{
-			if (curToken == NULL || curToken->type != ttKeywords || dynamic_cast<KeyWordToken*>(curToken.get())->kw != kw)
-			{
-				throw exception("expected another key word");
-			}
+			if (curToken == NULL || curToken->type != ttKeywords || dynamic_cast<KeyWordToken*>(curToken.get())->kw != kw) throw exception("expected another key word\n");
+			else getNext();
 		}
 		catch (const std::exception& exp)
 		{
 			cout << exp.what();
+			return false;
 		}
+		return true;
 	}
-	void accept(string vt)
+	bool accept(string name)
 	{
 		// For VariantType
 		try
 		{
-			if (curToken == NULL || curToken->type != ttConstants || get<string>(dynamic_cast<ConstToken*>(curToken.get())->data) != vt)
-			{
-				throw exception("expected another key word");
-			}
+			if (curToken == NULL || curToken->type != ttConstants || get<string>(dynamic_cast<ConstToken*>(curToken.get())->data) != name) throw exception("expected another variant type\n");
+			else getNext();
 		}
 		catch (const std::exception& exp)
 		{
 			cout << exp.what();
+			return false;
 		}
+		return true;
 	}
-	void program() {
-		accept("program");
-		name();
-		accept(ssSemicolon);
-		block();
-		accept(ssDot);
-	}
-	void name() { // name
+	bool accept_ident() { // name
 		try
 		{
-			if (curToken != NULL) get<string>(dynamic_cast<ConstToken*>(curToken.get())->data);
+			if (curToken == NULL || curToken ->type != ttIdentifier) throw exception("ident exp");
+			else curToken = lexer->getNextToken();
 		}
 		catch (const std::exception&)
 		{
 			throw exception("expected another key word");
+			return false;
 		}
+		return true;
+	}
+	void program() {
+		getNext();
+		accept(kwProgram);
+		accept_ident();
+		accept(ssSemicolon);
+		block();
+		accept(ssDot);
 	}
 	void block() { // block
+		accept(kwBegin);
 		var_block();
 		op_block();
 	}
-	void var_block() { // variabels block
-		accept("var");
+	void var_block() { // variables block
+		accept(kwVar);
 		same_var();
-		// { same_var() }
 		accept(ssSemicolon);
 	}
-	void same_var() { // odnotipnie variables
-
+	void same_var() { // same variables
+		accept_ident();
+		while (accept(ssComma))
+		{
+			accept_ident();
+		}
+		accept(ssColon);
+		type();
+	}
+	void type() { // types
+		accept_ident();
 	}
 	void op_block() { // operators block
 		comp_op();
 	}
 	void comp_op() { // sostavnoi operator
-		///// ! 
+
 	}
-	//bool isOper(List<Oper> ops)
-	//{
-	//	if (curToken == null || curToken.tokenType != ToketType.ttOper) return false;
-	//	foreach(var op in ops)
-	//	{
-	//		if (curToken.operation == op) return true;
-	//	}
-	//	return false;
-	//}
 
-	//float factor() // mnozitel
-	//{
-	//	if (curToken != null && curToken.tokenType == ToketType.ttOper)
-	//	{
-	//		accept(Oper.oLeftBracet);
-	//		var left = simpleExpr();
-	//		accept(Oper.oRightBracet);
-	//		return left;
-	//	}
-	//	if (curToken == null) throw new Exception("Token excpected");
-	//	var res = curToken.value;
-	//	getNext();
-	//	return res;
-	//}
-
-	//float term() //slagaemoe
-	//{
-	//	var left = factor();
-	//	while (isOper(new List<Oper>() { Oper.oMul, Oper.oDiv }))
-	//	{
-	//		var oper = curToken.operation;
-	//		getNext();
-	//		var right = factor();
-	//		switch (oper)
-	//		{
-	//		case Oper.oMul:
-	//			left = left * right;
-	//			break;
-	//		case Oper.oDiv:
-	//			left = left / right;
-	//			break;
-	//		default:
-	//			throw new Exception("bad multipl oper");
-	//		}
-	//	}
-	//	return left;
-	//}
-
-	//float simpleExpr()
-	//{
-	//	var left = term();
-	//	while (isOper(new List<Oper>() { Oper.oPlus, Oper.oMinus }))
-	//	{
-	//		var oper = curToken.operation;
-	//		getNext();
-	//		var right = term();
-	//		switch (oper)
-	//		{
-	//		case Oper.oPlus:
-	//			left = left + right;
-	//			break;
-	//		case Oper.oMinus:
-	//			left = left - right;
-	//			break;
-	//		default:
-	//			throw new Exception("bad addit oper");
-	//		}
-	//	}
-	//	return left;
-	// }
 };
