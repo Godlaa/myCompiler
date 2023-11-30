990c94df9d43d09a93082596375c5c2f1412ebdb
Эстокадо Фамилио <mimimilka55@gmail.com>
2023-11-30 13:02:30 +0500

    neutrolization

--- Syntax.h
+++ Syntax.h
@@ -15,22 +15,9 @@ public:
 private:
 	shared_ptr<Token> curToken;
 	shared_ptr<Lexer> lexer;
-	unordered_set<eKeyWords> st_block = { kwBegin, kwVar };
-	unordered_set<eKeyWords> st_var = { kwBegin, kwVar };
-	unordered_set<eSpecialSymbols> after_var = { ssSemicolon };
-	unordered_set<eTokenType> id_starters = { ttIdentifier };
-	void error(int index) {
-		switch (index)
-		{
-		case 1:
-			cout << "ошибка в разделе описаний\n"; /* ошибка в разделе описаний */
-			break;
-		default:
-			cout << "XZ\n";
-			break;
-		}
-	}
-	void getNext() {
+	// хватит ловить в try catch и например скипать до [then else] в блоке if 
+	void getNext() 
+	{
 		this->curToken = this->lexer->getNextToken();
 	}
 	eSpecialSymbols get_spec() {
@@ -65,244 +52,282 @@ private:
 	}
 	void accept(eSpecialSymbols ss)
 	{
-		// For SpecialSumbols
-		try
-		{
-			if (curToken == NULL || curToken->type != ttSpecialSymbols || get_spec() != ss) throw exception("expected spec symbol op\n");
-			else getNext();
-		}
-		catch (const std::exception& exp)
-		{
-			cout << exp.what();
-		}
+		if (curToken == NULL || curToken->type != ttSpecialSymbols || get_spec() != ss) throw exception("expected spec symbol op\n");
+		else getNext();
 	}
 	void accept(eKeyWords kw)
 	{
-		// For KeyWords
-		try
-		{
-			if (curToken == NULL || curToken->type != ttKeywords || get_keyword() != kw) throw exception("expected another key word\n");
-			else getNext();
-		}
-		catch (const std::exception& exp)
-		{
-			cout << exp.what();
-		}
+		if (curToken == NULL || curToken->type != ttKeywords || get_keyword() != kw) throw exception("expected another key word\n");
+		else getNext();
 	}
 	void accept(string name)
 	{
-		// For VariantType
-		try
-		{
-			if (curToken == NULL || curToken->type != ttConstants || get_constant() != name) throw exception("expected another variant type\n");
-			else getNext();
-		}
-		catch (const std::exception& exp)
-		{
-			cout << exp.what();
-		}
+		if (curToken == NULL || curToken->type != ttConstants || get_constant() != name) throw exception("expected another variant type\n");
+		else getNext();
+	}
+	void accept(eTokenType tt)
+	{
+		if (curToken == NULL || tt != curToken -> type) throw exception("expected another type\n");
+		else getNext();
 	}
 	void accept_ident()  // ident 
 	{
-		try
-		{
-			if (curToken == NULL || curToken ->type != ttIdentifier) throw exception("ident exp");
-			else getNext();
-		}
-		catch (const std::exception&)
-		{
-			cout << "exception ident";
-			getNext();
-		}
+		if (curToken == NULL || curToken ->type != ttIdentifier) throw exception("ident exp");
+		else getNext();
 	}
-	bool belong(unordered_set<eKeyWords> starters)
-	{
-		for (const auto& starter : starters) {
-			if (curToken != NULL && curToken->type == ttKeywords && get_keyword() == starter) {
+	bool belong(vector<variant<eSpecialSymbols, eKeyWords>> starters) {
+		for (const auto& starter : starters) 
+		{
+			if (curToken && (curToken->type == ttSpecialSymbols && holds_alternative<eSpecialSymbols>(starter) && get_spec() == get<eSpecialSymbols>(starter)) || (curToken->type == ttKeywords && holds_alternative<eKeyWords>(starter) && get_keyword() == get<eKeyWords>(starter))) 
+			{
 				return true;
 			}
 		}
 		return false;
 	}
-	void skipto2(unordered_set<eKeyWords> starters, std::unordered_set<eKeyWords> followers)
+	void skipto(vector<variant<eSpecialSymbols, eKeyWords>> starters)
 	{
-		while (!belong(starters) && !belong(followers))
+		while (curToken && !belong(starters))
 		{
 			getNext();
 		}
-	}
-	void skipto(unordered_set<eKeyWords> followers)
-	{
-		while (!belong(followers))
-		{
-			getNext();
-		}
-	}
-	unordered_set<eKeyWords> set_disjunct(unordered_set<eKeyWords> starters, unordered_set<eKeyWords> followers)
-	{
-		unordered_set<eKeyWords>new_vec;
-		for (const auto& starter : starters) 
-		{
-			new_vec.insert(starter);
-		}
-		for (const auto& follower : followers)
-		{
-			new_vec.insert(follower);
-		}
-		return new_vec;
 	}
 	void program() {
-		unordered_set<eKeyWords>followers = { };
 		getNext();
-		accept(kwProgram);
-		accept_ident();
-		accept(ssSemicolon);
-		block(followers);
-		accept(ssDot);
-	}
-	void block(unordered_set<eKeyWords> followers)  // block
-	{
-		var_block(followers);
-		op_block(followers);
-		
-	}
-	void var_block(unordered_set<eKeyWords> followers) /* анализ конструкции <раздел переменных> */
-	{ 
-		if (!belong(st_var))
+		try
 		{
-			error(1);
-			skipto2(st_var, followers);
+			accept(kwProgram);
+			accept_ident();
+			accept(ssSemicolon);
+			block();
+			accept(ssDot);
 		}
-		if (curToken ->	type == ttKeywords && get_keyword() == kwVar && belong(st_var))
+		catch (const std::exception& exp) { cout << exp.what(); }
+
+	}
+	void block( )  // block
+	{
+		try
+		{
+			var_block();
+			op_block();
+		}
+		catch (const std::exception& exp) { skipto(vector<variant<eSpecialSymbols, eKeyWords>> { kwVar, kwBegin }); cout << exp.what(); }
+	}
+	void var_block() /* анализ конструкции <раздел переменных> */
+	{ 
+		try
 		{
 			accept(kwVar);
-			same_var(followers);
-			accept(ssSemicolon);
+			do
+			{
+				same_var();
+				accept(ssSemicolon);
+			} while (curToken->type == ttIdentifier);
 		}
-	}
-	void same_var(unordered_set<eKeyWords>followers) /* анализ конструкции <описание однотипных переменных> */
-	{ 
-		accept_ident();
-		while (get_spec() == ssComma)
+		catch (const std::exception& exp)
 		{
-			accept(ssComma);
+			skipto(vector<variant<eSpecialSymbols, eKeyWords>> { kwBegin }); cout << exp.what();
+		}
+
+	}
+	void same_var() /* анализ конструкции <описание однотипных переменных> */
+	{ 
+		try
+			{
 			accept_ident();
+			while (curToken -> type == ttSpecialSymbols && get_spec() == ssComma)
+			{
+				accept(ssComma);
+				accept_ident();
+			}
+			accept(ssColon);
+			type();
 		}
-		accept(ssColon);
-		type(followers);
+		catch (const std::exception& exp)
+		{
+			skipto(vector<variant<eSpecialSymbols, eKeyWords>> { ssSemicolon, kwBegin }); cout << exp.what();
+		}
 	}
-	void type(unordered_set<eKeyWords>followers)
+	void type()
 	{ 
 		accept_ident();
 	}
- 	void op_block(unordered_set<eKeyWords>followers)
+ 	void op_block()
 	{
-		compoundstatement(followers);
+		compoundstatement();
 	}
-	void compoundstatement(unordered_set<eKeyWords>followers) /* анализ конструкции <составной оператор> */
+	void compoundstatement() /* анализ конструкции <составной оператор> */
 	{ 
-		accept(kwBegin); 
-		statement(followers);
-		while (curToken->type == ttSpecialSymbols && get_spec() == ssSemicolon)
+		try
 		{
-			accept(ssSemicolon); 
-			statement(followers);
+			accept(kwBegin);
+			statement();
+			while (curToken->type == ttSpecialSymbols && get_spec() == ssSemicolon)
+			{
+				accept(ssSemicolon);
+				statement();
+			}
+			accept(kwEnd);
 		}
-		accept(kwEnd);
+		catch (const std::exception& exp)
+		{
+			skipto(vector<variant<eSpecialSymbols, eKeyWords>> { ssDot }); cout << exp.what();
+		}
 	}
-	void whilestatement(unordered_set<eKeyWords>followers) /* анализ конструкции <цикл с предусловием> */
+	void whilestatement() /* анализ конструкции <цикл с предусловием> */
 	{
-		accept(kwWhile); 
+		try
+		{
+			accept(kwWhile);
+		}
+		catch (const std::exception& exp)
+		{
+			cout << exp.what();
+		}
 		expression();
-		accept(kwDo); 
-		statement(followers);
+		try
+		{
+			accept(kwDo);
+			statement();
+		}
+		catch (const std::exception& exp)
+		{
+			skipto(vector<variant<eSpecialSymbols, eKeyWords>> { kwWhile, kwFor, kwIf }); cout << exp.what();
+		}
 	}
 	void variable() /* анализ конструкции <переменная> */
 	{
-		accept_ident();
-		while (get_spec() == ssLeftCurveBrascet || get_spec() == ssDot)
-		{ 
-			switch (get_spec())
+		try
+		{
+			accept_ident();
+			if (curToken->type == ttSpecialSymbols)
 			{
-			case ssLeftCurveBrascet:
-				accept(ssLeftCurveBrascet); 
-				expression();
-				while (get_spec() == ssComma)
+				while (get_spec() == ssLeftCurveBrascet || get_spec() == ssDot)
 				{
-					accept(ssComma); 
-					expression();
+					switch (get_spec())
+					{
+					case ssLeftCurveBrascet:
+						accept(ssLeftCurveBrascet);
+						expression();
+						while (get_spec() == ssComma)
+						{
+							accept(ssComma);
+							expression();
+						}
+						accept(ssRightCurveBrascet);
+						break;
+					case ssDot:
+						accept(ssDot);
+						accept_ident();
+						break;
+					}
 				}
-				accept(ssRightCurveBrascet);
-				break;
-			case ssDot:
-				accept(ssDot);
-				accept_ident();
-				break;
 			}
 		}
-	}
-	void ifstatement(unordered_set<eKeyWords>followers) /* анализ конструкции <условный оператор> */
-	{
-		accept(kwIf);
-		expression();
-		accept(kwThen); 
-		statement(followers);
-		if (curToken->type == ttKeywords && get_keyword() == kwElse)
+		catch (const std::exception& exp)
 		{
-			accept(kwElse);
-			statement(followers);
+			skipto(vector<variant<eSpecialSymbols, eKeyWords>> { ssAssigment, kwElse }); cout << exp.what();
 		}
 	}
-	void forstatement(unordered_set<eKeyWords>followers) /* анализ конструкции <цикл с параметром> */
+	void ifstatement() /* анализ конструкции <условный оператор> */
 	{
-		accept(kwFor); 
-		accept_ident();
-		accept(ssAssigment);
-		expression();
-		if (curToken->type == ttKeywords && get_keyword() == kwTo || get_keyword() == kwToDownTo) 
+		try
 		{
-			getNext();
-			expression(); 
-			accept(kwDo);
-			statement(followers);
+			accept(kwIf);
+			expression();
+			accept(kwThen);
+			statement();
 		}
-	}
-	void statement(unordered_set<eKeyWords>followers) /* анализ конструкции <оператор> */
-	{
-		switch (curToken -> type)
+		catch (const std::exception& exp)
 		{
-		case ttIdentifier:
-			assignment();
-			break;
-		case ttKeywords:
-			switch (get_keyword())
+			skipto(vector<variant<eSpecialSymbols, eKeyWords>> { kwElse }); cout << exp.what();
+		}
+		try
+		{
+			if (curToken->type == ttKeywords && get_keyword() == kwElse)
 			{
-			case kwBegin:
-				compoundstatement(followers); break;
-			case kwIf:
-				ifstatement(followers); break;
-			case kwWhile:
-				whilestatement(followers); break;
-			case kwFor:
-				forstatement(followers); break;
-			case kwEnd:
-				break;
-			case kwUntil:
-				break;
-			case kwElse: break; /* в случае пустого оператора */
+				accept(kwElse);
+				statement();
 			}
-			break;
+		}
+		catch (const std::exception& exp)
+		{
+			skipto(vector<variant<eSpecialSymbols, eKeyWords>> { kwEnd }); cout << exp.what();
+		}
+	}
+	void forstatement() /* анализ конструкции <цикл с параметром> */
+	{
+		try
+		{
+			accept(kwFor);
+			accept_ident();
+			accept(ssAssigment);
+			expression();
+			if (curToken->type == ttKeywords && get_keyword() == kwTo || get_keyword() == kwToDownTo)
+			{
+				getNext();
+				expression();
+				accept(kwDo);
+				statement();
+			}
+		}
+		catch (const std::exception& exp)
+		{
+			skipto(vector<variant<eSpecialSymbols, eKeyWords>> { kwEnd }); cout << exp.what();
+		}
+	}
+	void statement() /* анализ конструкции <оператор> */
+	{
+		try
+		{
+			switch (curToken->type)
+			{
+			case ttIdentifier:
+				assignment();
+				break;
+			case ttKeywords:
+				switch (get_keyword())
+				{
+				case kwBegin:
+					compoundstatement(); break;
+				case kwIf:
+					ifstatement(); break;
+				case kwWhile:
+					whilestatement(); break;
+				case kwFor:
+					forstatement(); break;
+				case kwEnd:
+					break;
+				case kwUntil:
+					break;
+				case kwElse: break; /* в случае пустого оператора */
+				}
+				break;
+			}
+		}
+		catch (const std::exception& exp)
+		{
+			skipto(vector<variant<eSpecialSymbols, eKeyWords>> { kwEnd });
+			cout << exp.what();
 		}
 	}
 	void assignment()
 	{
-		variable();
-		accept(ssAssigment);
-		expression();
+		try
+		{
+			variable();
+			accept(ssAssigment);
+			expression();
+		}
+		catch (const std::exception& exp)
+		{
+			skipto(vector<variant<eSpecialSymbols, eKeyWords>> { ssSemicolon });
+			cout << exp.what();
+		}
 	}
 	void expression() 
 	{
-		eSpecialSymbols oper;
 		simple_expression();
 		if (curToken->type == ttSpecialSymbols && realation_operator())
 		{
@@ -335,15 +360,18 @@ private:
 			accept(ssLeftCurveBrascet);
 			expression();
 			accept(ssRightCurveBrascet);
+			// map <string, CType> при разборе var - 's' , string
+			// приведение типов функция compatible to
+			// функция op возможна ли операция между переменными
 		}
 		else
 		{
 			switch (curToken->type) {
 			case ttConstants:
-				getNext(); // ПОЛУЧИЛИ Константу 
+				accept(ttConstants);
 				break;
 			case ttIdentifier:
-				getNext(); // ПОЛУЧИЛИ ПЕРЕМЕННУЮ ТИПО ПРОСТО 5 ИЛИ 'СТРОКА' И ЧЕ С ЭТИМ ДЕЛАТЬ
+				accept(ttIdentifier);
 				break;
 			}
 		}
